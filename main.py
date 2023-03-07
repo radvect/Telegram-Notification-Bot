2fed3ab72c29cc22e97230f5d0224424869cfa08
radvect <pavel.buklemishev@gmail.com>
2023-03-07 23:47:27 +0200

    The description of previous commit wasn't fully right. This commit implements the /start hello function

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import telebot
+from telebot import types
+with open("venv/token.txt","r") as f:
+    token = f.readline();
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+bot=telebot.TeleBot(token)
+@bot.message_handler(commands=['start'])
+def start_message(message):
+	bot.send_message(message.chat.id,'Shalom! This bot will help you to find the flat of your dream')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+bot.polling()
