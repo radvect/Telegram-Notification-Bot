da873a4af0779f02c87331bd6a175ee5ab865111
radvect <pavel.buklemishev@gmail.ru>
2023-03-11 22:58:34 +0200

    Added the database to the project.
    Added function that inserts into the DB the user_id during the start.
    
    Added some prototypes for the further functions

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import telebot
+import database_init
 from telebot import types
 with open("venv/token.txt","r") as f:
-    token = f.readline();
+    token = f.readline()
 
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
 	bot.send_message(message.chat.id,'Shalom! This bot will help you to find the flat of your dream')
+	bot.send_message(message.chat.id,'Choose from the /city /space /price /room /dud /conditioner')
+	id = message.from_user.id
+	print(id)
+	database_init.db_init_user(id)
 
 
 bot.polling()
